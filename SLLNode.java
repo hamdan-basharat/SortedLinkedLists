ba55988ece223214b80184443e9f76a57fd4b560
hamdan-basharat <80228201+hamdan-basharat@users.noreply.github.com>
2021-07-24 11:44:06 -0400

    Add files via upload

---
+++ SLLNode.java
@@ -0,0 +1,11 @@
+package lab5practice;
+
+public class SLLNode {
+    int value;
+    SLLNode next;
+    public SLLNode(int value, SLLNode next)
+    {
+        this.value = value;
+        this.next = next;
+    }
+}
